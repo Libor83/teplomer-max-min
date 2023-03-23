0570a2ca182557d144585c139760216172918d62
Libor83 <126621465+Libor83@users.noreply.github.com>
2023-03-23 17:44:56 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+})
+let min = 0
+let max = 0
+let aktuální_teplota = input.temperature()
+max = aktuální_teplota
+min = aktuální_teplota
+basic.forever(function () {
+    basic.showString(".")
+    aktuální_teplota = input.temperature()
+    if (aktuální_teplota < min) {
+        min = aktuální_teplota
+    } else if (aktuální_teplota > max) {
+        max = aktuální_teplota
+    } else {
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.pause(1000)
+    }
+})
